87e1e888c0e5db901f0d14368a2508037e959afb
Brock <Lynch.b@hotmail.com>
2022-07-19 10:08:23 -0400

    inital commit

--- js/site.js
+++ js/site.js
@@ -1,14 +1,68 @@
-function getValue(message){
-    alert("HELLO WOLRD!");
+function getNumbers(){
+    let start = document.getElementById("startValue").value;
+    let end = document.getElementById("endValue").value;
+
+    start = parseInt(start);
+    end = parseInt(end);
+
+    if (Number.isInteger(start) && Number.isInteger(end)) {
+        let numbers = generateNumbers(start,end);
+        displayValuesRev(numbers);
+    }else{
+       Swal.fire({
+        backdrop: false,
+        title: "oops!",
+        text: "You can only enter numbers."
+       });
+    }
 }
-function getValue2(){
-    let msg = '';
-    msg = document.getElementById("message").value;
-    swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
+// generates numbers
+// buessness logic
+function generateNumbers(start, stop){
+    let numbers = [];
+    for(let i=start; i<=stop; i++){
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+// displays a list of numbers
+// bold the even numbers
+function displayValues(value){
+    results = document.getElementById("results");
+    results.innerHTML="";
+
+    for(let i=0; i < value.length; i++){
+        let divTag = document.createElement("div");
+        divTag.classList.add("col");
+        divTag.innerHTML = value[i];
+           
+        if(value[i]%2==0){
+            divTag.classList.add("even");
+            results.appendChild(divTag);
+        }else{
+            divTag.classList.add("odd");
+            results.appendChild(divTag);
         }
-    );
+    }
 }
+
+function displayValuesRev(value){
+    results = document.getElementById("results");
+    results.innerHTML="";
+
+    for(let i=value.length-1; i >= 0; i--){
+        let divTag = document.createElement("div");
+        divTag.classList.add("col");
+        divTag.innerHTML = value[i];
+           
+        if(value[i]%2==0){
+            divTag.classList.add("even");
+            results.appendChild(divTag);
+        }else{
+            divTag.classList.add("odd");
+            results.appendChild(divTag);
+        }
+    }
+}
+
